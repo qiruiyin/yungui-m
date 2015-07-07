f8bf7a54cae651548c987b8db76a53a3c07c8fa6
戚瑞印 <qiruiyin@qiruiyindeMacBook-Air.local>
2015-07-07 15:15:13 +0800

    img-jpeg: 图片改为jpg格式

--- app/js/index.js
+++ app/js/index.js
@@ -2,17 +2,31 @@
   'use strict';
 
   /* code here! */
+  var nav = document.querySelector('nav'),
+      nav_l = document.querySelector('.nav');
+      
+  document.addEventListener('click', function(){
+    nav_l.className = 'nav';
+  });
+
+  nav.addEventListener('click', function(e){
+    e.stopPropagation();
+    nav_l.className == 'nav' ? nav_l.className += ' active' : nav_l.className = 'nav';
+  });
+
   // 获取导航条
-  var nav = $('nav'),
-  		nav_l = $('.nav');
+  // var nav = $('nav'),
+  // 		nav_l = $('.nav');
 
-  $('main').click(function(){
-  	$('.nav').hasClass('active') ? $('.nav').removeClass('active') : ' ';
-  });
+  // $('main').click(function(){
+  // 	$('.nav').hasClass('active') ? $('.nav').removeClass('active') : ' ';
+  // });
 
-  nav.click(function(e){
-  	e.stopPropagation();
-  	nav_l.toggleClass('active');
-  });
+  // nav.click(function(e){
+  // 	e.stopPropagation();
+  // 	nav_l.toggleClass('active');
+  // });
 
 })();
+
+
