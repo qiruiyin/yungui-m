5e8505ddec4f8693d15807f448e17d1d76138078
戚瑞印 <qiruiyin@qiruiyindeMacBook-Air.local>
2015-07-17 15:43:25 +0800

    new: 新版手机页面

--- app/js/index.js
+++ app/js/index.js
@@ -2,31 +2,82 @@
   'use strict';
 
   /* code here! */
-  var nav = document.querySelector('nav'),
-      nav_l = document.querySelector('.nav');
-      
-  document.addEventListener('click', function(){
-    nav_l.className = 'nav';
+  // 首页的轮播图
+  var navSwiper = new Swiper('#slide',{
+    pagination : '.slide-pagination',
+    loop: true,
+    autoplay: 1500,//可选选项，自动滑动
+    //pagination : '#swiper-pagination1',
   });
 
-  nav.addEventListener('click', function(e){
+  var mySwiper = new Swiper('#news',{
+    pagination : '.news-pagination',
+    loop: true,
+    autoplay: 1200,//可选选项，自动滑动
+    //pagination : '#swiper-pagination1',
+  });
+
+  // 菜单部分
+  $('nav').on('touchend', function(e){
     e.stopPropagation();
-    nav_l.className == 'nav' ? nav_l.className += ' active' : nav_l.className = 'nav';
+    $('.has-child').removeClass('active');
+    $('.nav-child').removeClass('active');
+    $('.nav').toggleClass('active');
   });
 
-  // 获取导航条
-  // var nav = $('nav'),
-  // 		nav_l = $('.nav');
+  $('[data-link]').on('touchend', function(){
+    var link_value = $(this).attr('data-link'),
+        link_id = $(this).attr('data-linkId'),
+        href = '';
+    href = link_id ? link_value + '.html#' + link_id : link_value + '.html'; 
+    location.href = href;
+    // link_id ? $('body').scrollTop(-100): '';
+  });
 
-  // $('main').click(function(){
-  // 	$('.nav').hasClass('active') ? $('.nav').removeClass('active') : ' ';
-  // });
+  $('.has-child').on('touchend',function(e){
+    $(this).siblings('.has-child').removeClass('active');
+    $(this).toggleClass('active');
+    $(this).siblings('.has-child').next().removeClass('active');
+    $(this).next().toggleClass('active');
+  });
 
-  // nav.click(function(e){
-  // 	e.stopPropagation();
-  // 	nav_l.toggleClass('active');
-  // });
+  $('.nav').on('touchend', function(e){
+    e.stopPropagation();
+  });
+  
+  $('body').on('touchend', function(){
+    $('.has-child').removeClass('active');
+    $('.nav-child').removeClass('active');
+    $('.nav').removeClass('active');
+  });
+
+  // 下拉加载
+  var num = 3;
+  var sum = $('.scroll').children().length;
+  $(window).scroll(function(){
+   if(num == sum-1){
+      $('.scroll-more').html("全部加载");
+    } else {
+      var scrollTop = $(this).scrollTop();
+      var scrollHeight = $(document).height();
+      var windowHeight = $(this).height();
+    　if(scrollTop + windowHeight + 0 >= scrollHeight){
+        $('.scroll-more i').addClass('active');
+        setInterval(function(){
+          $('.scroll-more i').removeClass('active');
+          if(num <= sum){
+            $('.scroll > :eq('+ num + ')').show();
+          }
+        },1000);
+        num++;
+      }
+  　}
+  });
+
+  // 判断hash值是否存在
+  if(location.hash){
+    var offset_top = $(location.hash).offset().top - 58;
+    $('html,body').stop().animate({scrollTop:offset_top});
+  }
 
 })();
-
-
---
+++ app/js/map.js
@@ -0,0 +1,69 @@
+//创建和初始化地图函数：
+    function initMap(){
+      createMap();//创建地图
+      setMapEvent();//设置地图事件
+      addMapControl();//向地图添加控件
+      addMapOverlay();//向地图添加覆盖物
+    }
+    function createMap(){ 
+      map = new BMap.Map("map"); 
+      map.centerAndZoom(new BMap.Point(121.40249,31.230676),19);
+    }
+    function setMapEvent(){
+      map.enableScrollWheelZoom();
+      map.enableKeyboard();
+      map.enableDragging();
+      map.enableDoubleClickZoom()
+    }
+    function addClickHandler(target,window){
+      target.addEventListener("click",function(){
+        target.openInfoWindow(window);
+      });
+    }
+    function addMapOverlay(){
+      var markers = [
+      ];
+      for(var index = 0; index < markers.length; index++ ){
+        var point = new BMap.Point(markers[index].position.lng,markers[index].position.lat);
+        var marker = new BMap.Marker(point,{icon:new BMap.Icon("http://api.map.baidu.com/lbsapi/createmap/images/icon.png",new BMap.Size(20,25),{
+          imageOffset: new BMap.Size(markers[index].imageOffset.width,markers[index].imageOffset.height)
+        })});
+        var label = new BMap.Label(markers[index].title,{offset: new BMap.Size(25,5)});
+        var opts = {
+          width: 200,
+          title: markers[index].title,
+          enableMessage: false
+        };
+        var infoWindow = new BMap.InfoWindow(markers[index].content,opts);
+        marker.setLabel(label);
+        addClickHandler(marker,infoWindow);
+        map.addOverlay(marker);
+      };
+      var labels = [
+      ];
+      for(var index = 0; index < labels.length; index++){
+        var opt = { position: new BMap.Point(labels[index].position.lng,labels[index].position.lat )};
+        var label = new BMap.Label(labels[index].content,opt);
+        map.addOverlay(label);
+      };
+      var plOpts = [
+      ];
+      var plPath = [
+      ];
+      for(var index = 0; index < plOpts.length; index++){
+        var polyline = new BMap.Polyline(plPath[index],plOpts[index]);
+        map.addOverlay(polyline);
+      }
+    }
+    //向地图添加控件
+    function addMapControl(){
+      var scaleControl = new BMap.ScaleControl({anchor:BMAP_ANCHOR_BOTTOM_RIGHT});
+      scaleControl.setUnit(BMAP_UNIT_IMPERIAL);
+      map.addControl(scaleControl);
+      var navControl = new BMap.NavigationControl({anchor:BMAP_ANCHOR_TOP_LEFT,type:BMAP_NAVIGATION_CONTROL_LARGE});
+      map.addControl(navControl);
+      var overviewControl = new BMap.OverviewMapControl({anchor:BMAP_ANCHOR_BOTTOM_RIGHT,isOpen:false});
+      map.addControl(overviewControl);
+    }
+    var map;
+      initMap();
